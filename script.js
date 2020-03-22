6722314fa5d0bdc536aac5400398766aa2ea11aa
Eisendrach <manuel.barbieri16@gmail.com>
2020-03-22 10:17:33 +0100

    Primo_tentativo

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const button = document.getElementById("incrementa-btn")
-const counter = document.getElementById("contatore-lbl")
+const button1 = document.getElementById('bottone_1')
+const button2 = document.getElementById('bottone_2')
+const button3 = document.getElementById('bottone_3')
+const button4 = document.getElementById('bottone_4')
 
-let counterValue = 0
-const increment = () => {
-  counterValue ++
-  counter.innerHTML = counterValue
+const selezione = () => {
+
+	const lista = ["Sasso", "Carta", "Forbice"]
+	const scelta =lista[Math.floor(Math.random() * 3)];
+	return(scelta)
 }
-
-button.onclick = increment
+console.log(selezione()
