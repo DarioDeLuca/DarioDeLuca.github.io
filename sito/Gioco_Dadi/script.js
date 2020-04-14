7b8210f3e39c6ac55f275800f5f5eff3cd7581a4
Eisendrach <manuel.barbieri16@gmail.com>
2020-04-14 13:26:07 +0200

    Completato lo script del gioco dei dadi; manca la grafica ed un eventuale display per comunicare i messaggi stampati nella console

--- sito/Gioco_Dadi/script.js
+++ sito/Gioco_Dadi/script.js
@@ -3,36 +3,59 @@ const bottone_scelta=document.getElementById("bottone_2")
 let somma=0
 let contatore=0
 let valore=0
-let scelta_pc=0
-const obbiettivo=100
+let scelta_pc=0 
+const obbiettivo=70
 
 
-function LancioDadi(){
-    if(somma===obbiettivo){
-        Verifica(valore, scelta_pc,contatore)
+function LancioDadi(){ 
+    if(somma > obbiettivo){
+        console.log("attualmente il contatore è "+ contatore)
+        Verifica(valore, contatore)
     }
-    else{    for(i=0;i<6;i++){
+    else{
+        contatore+=1    
+        for(i=0;i<6;i++){
          let lancio = Math.floor(Math.random() * 6) + 1
-        somma+=lancio 
-    }
-    contatore+=1
-    console.log("il totale dopo "+ contatore + " tiri è " + somma)  
+            somma+=lancio 
+        }
+        console.log("il totale dopo "+ contatore + " tiri è " + somma)  
     }
 }
 
-function Verifica (a, b, count){
-    if(a===count){
-        console.log("HAI VINTO")
+function ErroreCalcolato(){
+    scelta_pc=contatore
+    const k= Math.floor(Math.random() * 6)
+    const random=Math.floor(Math.random() * 2) +1
+    if(random===1){scelta_pc-=k}
+    else {scelta_pc+=k}
+    if(scelta_pc<0){scelta_pc= -scelta_pc}
+    console.log("Bene, abbiamo finito, avevo scommesso ci saresti riuscito in "+ scelta_pc + " tiri, vediamo chi si è  avviinato di più")
+    return(scelta_pc)
+}
+
+function Verifica (a, count){
+    let errore_giocatore=a-count
+    if (errore_giocatore < 0){
+        errore_giocatore = -errore_giocatore       
     }
-    if (b===count){
-        console.log("HAI PERSO")
+    const falso_errore = ErroreCalcolato()
+    console.log("il computer ha sbagliato di "+falso_errore)
+    console.log("il player ha sbagliato di "+errore_giocatore)
+    if(errore_giocatore < falso_errore){
+        console.log("Ottimo lavoro, hai vinto!")
+    }
+    else if(falso_errore < errore_giocatore){
+        console.log("A quanto pare avevo ragione io")
+    }
+    else{
+        console.log("parità")
     }
 }
 
 
 function Registra(){
-    const valore = document.getElementById("exampleInputPassword1").value
-    console.log(valore)
+    valore = document.getElementById("display_puntata").value
+    console.log("Ebbene, credi di farcela in "+ valore + " tiri?")
 }
 
 bottone_scelta.onclick=Registra    
