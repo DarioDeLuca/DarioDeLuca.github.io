a50d3a207ccdb2a0bd8b809c83e29c322b40e7a4
Dario <dario251102@gmail.com>
2020-04-17 08:32:25 +0200

    piccole correzioni finali

--- sito/Gioco_Dadi/dadi.js
+++ sito/Gioco_Dadi/dadi.js
@@ -3,6 +3,7 @@ const bottone_scelta=document.getElementById("bottone_2")
 const dialogo=document.getElementById("spazio_scritte")
 const dadi=["dado1.jpg","dado2.png","dado3.png","dado4.png","dado5.png","dado6.png",]
 const posizioni=["primodado","secondodado","terzodado","quartodado","quintodado","sestodado"]
+document.getElementById('bottone_1').disabled=true
 let somma=0
 let contatore=0
 let valore=0
@@ -52,20 +53,20 @@ function Verifica (a, count){
 	console.log("il player ha sbagliato di "+ errore_giocatore)
 	if(errore_giocatore < falso_errore){
 		console.log("Ottimo lavoro, hai vinto!")
-		dialogo.innerHTML="Ottimo lavoro, avevi ragione e sei riuscito a battermi, non avrei dovuto scommettere su "+scelta_pc+" tiri"
+		dialogo.innerHTML="Ottimo lavoro, avevi ragione e sei riuscito a battermi, non avrei dovuto scommettere su "+scelta_pc+" tiri!"
 		document.getElementById('bottone_1').disabled=true
 		document.getElementById('bottone_2').disabled=true
 	}
 	else if(falso_errore < errore_giocatore){
 		console.log("A quanto pare avevo ragione io")
-		dialogo.innerHTML="Peccato, non sei riuscito a vincere, sapevo avrei vinto puntando su "+scelta_pc+" tiri"
+		dialogo.innerHTML="Peccato, hai perso, sapevo avrei vinto puntando su "+scelta_pc+" tiri!"
 		document.getElementById('bottone_1').disabled=true
 		document.getElementById('bottone_2').disabled=true
 	}
 	if(a===scelta_pc){
 
 		console.log("parità")
-		dialogo.innerHTML="Impossibile?! Come hai fatto a capire volessi scommettere su "+scelta_pc+" tiri?"
+		dialogo.innerHTML="Impossibile?! Come hai fatto a capire volessi scommettere anche io su "+scelta_pc+" tiri?"
 		document.getElementById('bottone_1').disabled=true
 		document.getElementById('bottone_2').disabled=true
 	}
@@ -75,6 +76,8 @@ function Verifica (a, count){
 function Registra(){
 	valore = document.getElementById("display_puntata").value
 	let scelta=parseInt(valore)
+	document.getElementById('bottone_1').disabled=false
+	document.getElementById('bottone_2').disabled=true
 	if ((scelta/1)===scelta ){
 		console.log("Ebbene, credi di farcela in "+ valore + " tiri?")
 		dialogo.innerHTML="Ebbene, credi di farcela in "+ valore + " tiri? Perfetto, chi tra noi si avvicinerà maggiormente al numero esatto di tiri vincerà"
